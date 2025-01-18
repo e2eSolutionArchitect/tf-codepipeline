9ef9a10afe395767d64ec37f4430f8c3a9dcccc1
The Solution Architect <62712515+TheSolutionArchitect@users.noreply.github.com>
2025-01-18 08:22:52 -0500

    Update and rename versions.tf to providers.tf

--- providers.tf
+++ providers.tf
@@ -1,9 +1,13 @@
 terraform {
-  required_version = "~> 1.2.0"
+  required_version = "~> 1.10.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.5.0"
+      version = "5.76.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.3"
     }
   }
 }
